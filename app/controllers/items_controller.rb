714863e08a7e4341120bc46531be0b77654bbe3b
koginomama <konomamadeha@gmail.com>
2023-06-18 01:17:09 +0900

    データベース保存まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-
-    #@item = Item.new(items_params)
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def items_params
-    params.require(:items).permit(:item_name, :item_info, :item_price, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :image)
+    params.require(:item).permit(:item_name, :item_info, :item_price, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,17 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :itemcategory
-  belongs_to :itemsalesstatus
-  belongs_to :itemshippingfeestatus
-  belongs_to :itemprefecture
-  belongs_to :itemscheduleddelivery
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
 
-  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_category_id, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :item_sales_status_id, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :item_shipping_fee_status_id, numericality: { other_than: 0 , message: "can't be blank" } 
   validates :item_prefecture_id, numericality: { other_than: 0 , message: "can't be blank" } 
-  validates :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_scheduled_delivery_id, numericality: { other_than: 0 , message: "can't be blank" } 
   validates :item_name, :item_info, :item_price, :image,  presence: true
 
 end
