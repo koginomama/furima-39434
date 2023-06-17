04b199ca0ea0febaab6b0baad71ad44e729c9c33
koginomama <konomamadeha@gmail.com>
2023-06-17 23:59:38 +0900

    各パラメータの確認まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+
+    #@item = Item.new(items_params)
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:item_name, :item_info, :item_price, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :image)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :itemcategory
+  belongs_to :itemsalesstatus
+  belongs_to :itemshippingfeestatus
+  belongs_to :itemprefecture
+  belongs_to :itemscheduleddelivery
+
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_prefecture_id, numericality: { other_than: 0 , message: "can't be blank" } 
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :item_name, :item_info, :item_price, :image,  presence: true
+
 end
