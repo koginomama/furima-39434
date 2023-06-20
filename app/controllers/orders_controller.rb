2ba26d155b1edb8c76f447ce36d08b91ab6bbbcd
koginomama <konomamadeha@gmail.com>
2023-06-20 18:54:48 +0900

    入力データ確認まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
-  
+
   def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :city, :addresses, :addresses, :addresses, :addresses, :prefecture_id :order_id
+  attr_accessor :postal_code, :city, :addresses, :building, :phone_number, :prefecture_id, :order_id
   
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city, :addresses, :order_id
     validates :phone_number, numericality: {only_integer: true, message: 'is invalid'}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  end
 
     def save
 
