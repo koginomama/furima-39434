da32804c636b561160661c6a3c0eba896611e421
koginomama <konomamadeha@gmail.com>
2023-06-21 14:49:18 +0900

    データ保存から表示まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,24 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
+  private
+
+  def order_params
+    item = Item.find(params[:item_id])
+    params.require(:order_address).permit(:postal_code, :city, :addresses, :building, :phone_number, :prefecture_id, :order_id, ).merge(user_id: current_user.id, item_id: item.id)
+  end
 end
