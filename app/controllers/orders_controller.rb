141365c8285185df5f7f560c1af9f1f98d5d1305
koginomama <konomamadeha@gmail.com>
2023-06-22 20:08:55 +0900

    ログインしてないユーザーの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   before_action :check_sold_item, only: [:index, :create]
 
   def index
